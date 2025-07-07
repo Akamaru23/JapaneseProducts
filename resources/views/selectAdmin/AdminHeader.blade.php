1f55e764424cfbe7f339061e13b9954612dfa587
Akamaru36 <eiichiro0106@gmail.com>
2025-07-07 12:48:43 +0800

    修改一點

--- resources/views/selectAdmin/AdminHeader.blade.php
+++ resources/views/selectAdmin/AdminHeader.blade.php
@@ -1,22 +1,13 @@
 <div class="header">
-    @if(Request::is('2bVtbHzQ/admin/database'))
-        <h1>データベース管理</h1>
-    @elseif(Request::is('2bVtbHzQ/admin/uploadImages'))
-        <h1>データ追加</h1>
-    @elseif(Request::is('2bVtbHzQ/admin/comments'))
-        <h1>メッセージ確認</h1>
-    @elseif(Request::is('2bVtbHzQ/admin/top3rank'))
+    @if(Request::is('2bVtbHzQ/admin/database*'))
+        <h1>資料庫管理</h1>
+    @elseif(Request::is('2bVtbHzQ/admin/uploadImages*'))
+        <h1>增加資料</h1>
+    @elseif(Request::is('2bVtbHzQ/admin/comments*'))
+        <h1>確認訊息</h1>
+    @elseif(Request::is('2bVtbHzQ/admin/top3rank*'))
         <h1>更新Top3</h1>
-    @elseif(Request::is('2bVtbHzQ/admin/rank'))
+    @elseif(Request::is('2bVtbHzQ/admin/rank*'))
         <h1>更新各縣Top3</h1>
-    @else
-        <h1>管理画面</h1>
-        <nav>
-            <ul>
-                <li><a href="{{ route('adminDatabase') }}">データ検索</a></li>
-                <li><a href="{{ route('showImg') }}">データ追加</a></li>
-                <li><a href="{{ route('showComments') }}">メッセージ確認</a></li>
-            </ul>
-        </nav>
     @endif
 </div>
