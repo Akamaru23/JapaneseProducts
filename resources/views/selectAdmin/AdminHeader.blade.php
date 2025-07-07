6910afbdec6aab23d767b719aa6732905285b9ff
Akamaru36 <eiichiro0106@gmail.com>
2025-07-07 13:07:01 +0800

    修改header的簡介

--- resources/views/selectAdmin/AdminHeader.blade.php
+++ resources/views/selectAdmin/AdminHeader.blade.php
@@ -2,7 +2,7 @@
     @if(Request::is('2bVtbHzQ/admin/database*'))
         <h1>資料庫管理</h1>
     @elseif(Request::is('2bVtbHzQ/admin/uploadImages*'))
-        <h1>增加資料</h1>
+        <h1>新增資料</h1>
     @elseif(Request::is('2bVtbHzQ/admin/comments*'))
         <h1>確認訊息</h1>
     @elseif(Request::is('2bVtbHzQ/admin/top3rank*'))
